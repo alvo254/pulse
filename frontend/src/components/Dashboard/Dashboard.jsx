598f199cb74307d335de4dad23670c2c0b8fdbf1
alvo254 <alvinkhamron@gmail.com>
2023-10-17 10:06:09 +0300

    upload function in react

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,47 @@
 import React from 'react'
+import axios from 'axios';
 
 const Dashboard = () => {
+
+
+    const [file, setFile] = useState()
+
+    function handleChange(event) {
+      setFile(event.target.files[0])
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const url = 'http://localhost:3000/uploadFile';
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('fileName', file.name);
+        const config = {
+          headers: {
+            'content-type': 'multipart/form-data',
+          },
+        };
+        axios.post(url, formData, config).then((response) => {
+          console.log(response.data);
+        });
+    
+      }
+
   return (
     <div>Dashboard
-      <iframe
-  src="https://jupyterlite.github.io/demo/repl/index.html?kernel=python&toolbar=1"
-  width="100%"
-  height="500px"
->
-  </iframe>
+        <iframe
+        src="https://jupyterlite.github.io/demo/repl/index.html?kernel=python&toolbar=1"
+        width="100%"
+        height="500px"
+        >
+        </iframe>
+
+        <form onSubmit={handleSubmit}>
+          <h1>React File Upload</h1>
+          <input type="file" onChange={handleChange}/>
+          <button type="submit">Upload</button>
+        </form>
+
     </div>
   )
 }
