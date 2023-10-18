82eee5541181f9ac9ea9e94a8db8ab16de319278
femmyte <sanyaoluadefemi@gmail.com>
2023-10-18 17:26:36 +0100

    Created some reusable components
    Installed the needed dependencies
    Created the Signin and Signup pages

---
+++ frontend/src/app/(auth)/signup/page.jsx
@@ -0,0 +1,12 @@
+import Signup from '@/components/pages/Signup';
+import React from 'react';
+
+const SignupPage = () => {
+	return (
+		<div>
+			<Signup />
+		</div>
+	);
+};
+
+export default SignupPage;
