fe963fd0321aa410297b0690d2c394e51f28073b
Moshood <adamsayobami@gmail.com>
2023-10-19 13:39:36 +0100

    Designing the hero section of the landing page

--- frontend/src/components/pages/LandingPage/Header.jsx
+++ frontend/src/components/pages/LandingPage/Header.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Header = () => {
-	return <div>Header</div>;
+	return <div class="container mx-auto">
+		<div class=" w-1/2">
+<h1>
+	A all in one tool for Data Scientist!
+</h1>
+<p>
+	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid, consequatur temporibus iusto unde esse architecto, alias natus veritatis nisi saepe corporis obcaecati ratione amet deleniti, sequi excepturi ipsa blanditiis nulla?
+</p>
+<button> Try For Free</button>
+<button> Book Demo</button>
+		</div>
+		<div div class=" w-1/2">
+			<img src="C:\Users\adams\Desktop\Dev\CIL_Academy_final_project\pulse\frontend\src\components\pages\LandingPage\images\luke-chesser-JKUTrJ4vK00-unsplash.jpg" alt="" />
+
+
+		</div>
+
+	</div>;
 };
 
 export default Header;
