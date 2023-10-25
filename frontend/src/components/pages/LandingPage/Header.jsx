8dd9b84bc8f5869836778d4c54248ef80d2e83c6
AdamsTheCoder <moshoodadams2020@gmail.com>
2023-10-25 20:05:06 +0100

    Changes made by changing all Css Class attributes to ClassName

--- frontend/src/components/pages/LandingPage/Header.jsx
+++ frontend/src/components/pages/LandingPage/Header.jsx
@@ -8,23 +8,23 @@ import logo5 from './images/pics5.jpg'
 
 const Header = () => {
 	return <div class="container">
-		<div class="container ml-8 mr-12 flex mt-14">
-		<div class=" w-1/2">
+		<div className="container ml-8 mr-12 flex mt-14">
+		<div className=" w-1/2">
 <h1 className='text-slate-700 font-[600] text-[3.5rem] leading-normal hover:text-slate-800' >
 	An all in one tool for Data Scientist!
 </h1>
-<p class="mt-4 mr-24">
+<p className="mt-4 mr-24">
 	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid, consequatur temporibus iusto unde esse architecto amet deleniti, sequi excepturi ipsa blanditiis nulla?
 </p>
 
-<div class='flex'>
+<div className='flex'>
 <button className='block py-[1.19rem] px-[1.25rem] bg-slate-900 text-slate-50 hover:bg-slate-800 font-[600] text-[1.10rem] leading-normal mt-6'> Try For Free</button>
 
 <button className='ml-6 mt-6 hover:grayscale-0'> Book Demo</button>
 </div>
 
 		</div>
-		<div class="w-1/2 ml-4">
+		<div className="w-1/2 ml-4">
 			<Image className='w-355 h-42'
 			src={logo1}
 			alt='data analytics'
@@ -35,9 +35,9 @@ const Header = () => {
 
 		</div>
 
-<div class="container gap-4 flex ml-8 mr-10 mt-12 mb-12">
+<div className="container gap-4 flex ml-8 mr-10 mt-12 mb-12">
 		<div className=' w-1/3 justify-center text-center'>
-     <h1 class='mb-4 text-slate-700 font-[600] text-[1.5rem] leading-normal hover:text-slate-800'>Descriptive Analytics</h1>
+     <h1 className='mb-4 text-slate-700 font-[600] text-[1.5rem] leading-normal hover:text-slate-800'>Descriptive Analytics</h1>
 		 <Image className='width:15%; margin:20px 20px 50px;'
 			src={logo2}
 			alt='Descriptive Analytics'
@@ -48,7 +48,7 @@ const Header = () => {
 
 		</div>
 		<div className=' w-1/3 justify-center text-center'>
-         <h1 class='mb-4 text-slate-700 font-[600] text-[1.5rem] leading-normal hover:text-slate-800'>Predictive Analytics</h1>
+         <h1 className='mb-4 text-slate-700 font-[600] text-[1.5rem] leading-normal hover:text-slate-800'>Predictive Analytics</h1>
 		 <Image className='width:15%; margin:20px 20px 50px;'
 			src={logo3}
 			alt='Predictive Analytics'
@@ -59,7 +59,7 @@ const Header = () => {
 		</div>
 	
 		<div className=' w-1/3 justify-center text-center'>
-        <h1 class='mb-4 text-slate-700 font-[600] text-[1.5rem] leading-normal hover:text-slate-800'>Prescriptive Analytics</h1>
+        <h1 className='mb-4 text-slate-700 font-[600] text-[1.5rem] leading-normal hover:text-slate-800'>Prescriptive Analytics</h1>
 		 <Image className='width:15%; margin:20px 20px 50px;'
 			src={logo5}
 			alt='Prescriptive Analytics'
@@ -71,6 +71,6 @@ const Header = () => {
 	</div>
 
 	</div>;
-};
+}
 
 export default Header;
