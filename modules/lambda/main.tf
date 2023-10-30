f164ec3549e4eb1ca997c5890fa904cc092f9788
alvo254 <alvinkhamron@gmail.com>
2023-10-30 11:06:48 +0300

    lambda update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -117,11 +117,11 @@ resource "aws_iam_role" "tweets_process_lambda_role" {
 
 resource "aws_lambda_function" "tweets_lambda_processor" {
   function_name    = "tweetProcessor"
-  role             = aws_iam_role.tweets_process_lambda_role
+  role             = aws_iam_role.tweets_process_lambda_role.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.11"
   filename         = "${path.module}/tweet_transform/lambda.zip"
-  source_code_hash = filebase64sha256(data.archive_file.tweets_transform_lambda.output_path)
+  source_code_hash = filebase64sha256(data.archive_file.tweets_process_lambda.output_path)
   timeout          = 900
 
   vpc_config {
