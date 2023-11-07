dd1ec46dbfc9fffc56cc026500de05760be3d207
femmyte <sanyaoluadefemi@gmail.com>
2023-11-07 08:52:34 +0100

    Created query executions page to view recent queries that were ran on the Athena

---
+++ frontend/src/app/(dashboard)/query-execution/page.jsx
@@ -0,0 +1,12 @@
+import QueryExecutions from '@/components/AWSservice/QueryExecutions';
+import React from 'react';
+
+const Executions = () => {
+	return (
+		<div>
+			<QueryExecutions />
+		</div>
+	);
+};
+
+export default Executions;
