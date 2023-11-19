9005a3d5d272ac22fbc2b5b31395ec23a5e589de
Alvin <alvinkhamron@gmail.com>
2023-11-19 20:47:42 +0300

    update

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -3,9 +3,9 @@ resource "aws_sns_topic" "billing_alerts" {
   name = "BillingAlertsTopic"
 }
 
-# Define an email endpoint for SNS subscriptions
-resource "aws_sns_topic_subscription" "email_subscription" {
-  topic_arn = aws_sns_topic.billing_alerts.arn
-  protocol  = "email"
-  endpoint  = "youremail@example.com" # Replace with your email address
-}
+# # Define an email endpoint for SNS subscriptions
+# resource "aws_sns_topic_subscription" "email_subscription" {
+#   topic_arn = aws_sns_topic.billing_alerts.arn
+#   protocol  = "email"
+#   endpoint  = "youremail@example.com" # Replace with your email address
+# }
